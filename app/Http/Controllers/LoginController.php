53679e068fa906aca96e80c9f0617bf9ce5dd5b0
tomoatsu <tomoatsu213@gmail.com>
2021-05-20 09:37:56 +0900

    heroku-up

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,14 +8,17 @@ use App\Models\User;
 
 class LoginController extends Controller
 {
-   public function post(Request $request)
-   {
-       $items = User::where('email', $request->email)->first();
-       $isPasswordCheck = Hash::check($request->password, $items->password);
-       if ( $isPasswordCheck) {
-           return response()->json(['auth' => true], 200);
-       } else {
-           return response()->json(['auth' => false], 401);
-       }
-   }
+    public function post(Request $request)
+    {
+        $items = User::where('email', $request->email)->first();
+        $isPasswordCheck = Hash::check($request->password, $items->password);
+        if ($isPasswordCheck) {
+            return response()->json([
+                'auth' => true,
+                'data' => $items->id
+            ], 200);
+        } else {
+            return response()->json(['auth' => false], 401);
+        }
+    }
 }
