199c02215273f52347f8a6e8659c5d4d7c47007e
ともあつ <tomoatsu213@gmail.com>
2021-06-25 14:27:46 +0900

    pre-complete-commit5

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,19 +25,19 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         
-    // 管理者のみ許可
-    Gate::define('admin', function ($user) {
-        return $user->role === 100;
-    });
+    // // 管理者のみ許可
+    // Gate::define('admin', function ($user) {
+    //     return $user->role === 100;
+    // });
 
-    // 店舗代表者のみ許可
-    Gate::define('owner', function ($user) {
-        return $user->role === 50;
-    });
+    // // 店舗代表者のみ許可
+    // Gate::define('owner', function ($user) {
+    //     return $user->role === 50;
+    // });
 
-    // ユーザーのみ許可
-    Gate::define('user', function ($user) {
-        return $user->role === 1;
-    });
+    // // ユーザーのみ許可
+    // Gate::define('user', function ($user) {
+    //     return $user->role === 1;
+    // });
     }
 }
--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'user',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
